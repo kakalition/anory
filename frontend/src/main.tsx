659b0d3e8d190723e651f6af6c04a7718336f906
Kaka <kakalition@gmail.com>
2022-06-17 15:32:00 +0700

    Add Eslint, React Router, and TailwindCSS

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import App from './App';
+import './index.css';
+
+function Te() {
+  return (
+    <div className="flex justify-center items-center w-screen h-screen bg-sky-200">
+      Hey
+    </div>
+  );
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/te" element={<Te />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
+);
