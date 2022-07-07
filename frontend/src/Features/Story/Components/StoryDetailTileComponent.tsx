6d7e05bf6532696e67eb0c340517704d7185976b
Kaka <kakalition@gmail.com>
2022-07-07 17:40:00 +0700

    Use useLike hook in StoryDetailTileComponent

--- frontend/src/Features/Story/Components/StoryDetailTileComponent.tsx
+++ frontend/src/Features/Story/Components/StoryDetailTileComponent.tsx
@@ -1,12 +1,7 @@
-import { useContext, useState } from 'react';
-import { useToast } from '@chakra-ui/react';
-import _ from 'lodash';
-import { AuthContext } from '../../AuthenticationWrapper';
 import EyeIcon from '../../Component/Icons/EyeIcon';
 import OutlinedHeartIcon from '../../Component/Icons/OutlinedHeartIcon';
 import Spacer from '../../Utilities/Spacer';
-import DeleteLikeDataUseCase from '../../../UseCases/LikeData/DeleteLikeDataUseCase';
-import LikeUseCase from '../../../UseCases/LikeData/PostLikeUseCase';
+import useLike from '../../../Hooks/UseLike';
 
 type Params = {
   id: number,
@@ -22,59 +17,7 @@ export default function StoryDetailTileComponent(params: Params) {
     id, title, body, likeData, totalViews, uploadedAt,
   } = params;
 
-  const toast = useToast();
-  const user = useContext<any>(AuthContext);
-  const [totalLike, setTotalLike] = useState(likeData.length);
-  const [likeByMeData, setLikeByMeData] = useState<any>(
-    likeData.find((value) => value.likee_id === user.id),
-  );
-
-  const showFailedToast = (toastTitle: String) => {
-    toast({
-      title: toastTitle,
-      containerStyle: { width: '100%' },
-      duration: 2000,
-      status: 'error',
-    });
-  };
-
-  const likeStoryAction = () => {
-    setLikeByMeData({});
-    setTotalLike(totalLike + 1);
-
-    LikeUseCase.handle(
-      { id, type: 'stories' },
-      (response) => setLikeByMeData(response.data),
-      () => {
-        showFailedToast('Failed to like this story.');
-        setLikeByMeData(null);
-        setTotalLike(totalLike - 1);
-      },
-    );
-  };
-
-  const dislikeStoryAction = (likeId: any) => {
-    const temporaryLikeData = likeByMeData;
-    setLikeByMeData(null);
-    setTotalLike(totalLike - 1);
-
-    DeleteLikeDataUseCase.handle(
-      likeId,
-      null,
-      () => {
-        showFailedToast('Failed to dislike this story.');
-        setLikeByMeData(temporaryLikeData);
-        setTotalLike(totalLike + 1);
-      },
-    );
-  };
-
-  const onHeartClick: React.MouseEventHandler = () => {
-    const isAlreadyLike = !_.isEmpty(likeByMeData);
-
-    if (!isAlreadyLike) likeStoryAction();
-    else dislikeStoryAction(likeByMeData.id);
-  };
+  const [totalLike, isLikedByMe, onHeartClick] = useLike({ entityId: id, likeData, type: 'stories' });
 
   return (
     <div className="p-[1.5rem] w-full bg-white rounded-lg drop-shadow-sm ">
@@ -87,7 +30,7 @@ export default function StoryDetailTileComponent(params: Params) {
           <div className="flex flex-row items-center">
             <button
               type="button"
-              className={`w-8 h-8 ${_.isEmpty(likeByMeData) ? 'stroke-[#FF4033] stroke-2 fill-transparent' : 'fill-[#FF4033]'}`}
+              className={`w-8 h-8 ${isLikedByMe ? 'fill-[#FF4033]' : 'stroke-[#FF4033] stroke-2 fill-transparent'}`}
               onClick={onHeartClick}
             >
               <OutlinedHeartIcon />
